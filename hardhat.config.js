f637d46b72d382817b87ce0542b74d12b61e94b5
ethancrypto <jonathan.cryptoguru@gmail.com>
2022-08-18 10:29:13 +0900

    added bsc deployment option

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,12 +14,25 @@ module.exports = {
       url: `https://rpc.ftm.tools/`,
       accounts: [process.env.privateKey],
     },
+    bsctest: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
+      accounts: [process.env.privateKey],
+    },
+    bsc: {
+      url: `https://bsc-dataseed.binance.org/`,
+      accounts: [process.env.privateKey],
+    },
   },
   solidity: {
-    compilers: [
-      { version: "0.8.7" },
-      { version: "0.7.6" },
-      { version: "0.6.6" }
+    compilers: [{
+        version: "0.8.7"
+      },
+      {
+        version: "0.7.6"
+      },
+      {
+        version: "0.6.6"
+      }
     ]
   },
 };
